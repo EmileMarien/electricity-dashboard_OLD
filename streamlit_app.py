cf39abfba81708973d5921b1bda4be2f69865c60
EmileMarien <100191898+EmileMarien@users.noreply.github.com>
2024-06-01 21:09:39 +0200

    update buttons

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from datetime import datetime, timedelta, timezone
+from datetime import datetime, timedelta
 import pytz
 import time
 
@@ -10,6 +10,16 @@ st.set_page_config(
     page_title='Electricity Meter Dashboard',
     page_icon=':electric_plug:',  # This is an emoji shortcode. Could be a URL too.
 )
+
+# Hide Streamlit's default menu and footer using custom CSS
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 # Dummy function to simulate live electricity meter data
 @st.cache_data(ttl=60)
